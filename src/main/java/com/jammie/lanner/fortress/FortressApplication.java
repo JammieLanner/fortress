d924ee27738a3f88e9d969eb95107cc0ebaa3e3b
Jammie <891143858@qq.com>
2019-10-17 22:53:53 +0800

    Initial commit

---
+++ src/main/java/com/jammie/lanner/fortress/FortressApplication.java
@@ -0,0 +1,13 @@
+package com.jammie.lanner.fortress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FortressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jammie/lanner/fortress/FortressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jammie.lanner.fortress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
